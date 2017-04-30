0b0a5180fde68dd537d119c3923b6c530d8b5744
Aaron <amaxpierce@yahoo.com>
2017-04-30 14:58:54 -0500

    added gem and stamina tracking

--- js/char.js
+++ js/char.js
@@ -5,6 +5,11 @@ class player{
     constructor(name){
         this.gold = 0;
         this.name = name;
+        this.actionsLeft = ["move","normal","once"];
+        this.anytimesLeft = 3;
+        this.baseActions = ["move","normal"];
+        this.stamina = 0;
+        this.maxstamina = 0;
         this.add = (amt) => {
             this.gold += amt;
         }
--- js/index.js
+++ js/index.js
@@ -4,12 +4,13 @@
 
 let chars = [];
 
-
+let anytimesString;
 let updateChars = () => {
     "use strict";
     $(".main").html("");
     for(let i = 0; i < chars.length; i++){
         let color;
+        let stamColor;
         let current = chars[i];
 
         if(current.gold <= 0){
@@ -21,14 +22,74 @@ let updateChars = () => {
         }else if(current.gold > 1000){
             color = "orange"
         }
+
+        if(current.stamina <= 0){
+            stamColor = "red"
+        }else if(current.stamina > 0 && current.stamina < 10){
+            stamColor = "yellow"
+        }else if(current.stamina > current.maxstamina) {
+            stamColor = "purple";
+            $.notify(`Stamina of player ${current.name} is above their max stamina, which is ${current.maxstamina}. Press S to set maximum stamina`, "info");
+        }
+            else{
+            stamColor = "green"
+        }
+
+
+
+        let uid = "a" + Math.round(Math.random()* 10000000000);
+        let actionsString = ``;
+        for(let j = 0; j < current.actionsLeft.length; j++){
+            console.log(current.actionsLeft[j]);
+            actionsString = actionsString +  `<img class="gem ${uid}" id="${current.actionsLeft[j]}" src="./fonts/${current.actionsLeft[j]}.png" >`;
+        }
+        let anytimesString = ``;
+            for(let pizza = 0; pizza < current.anytimesLeft; pizza++){
+                anytimesString += `<img class="anytime" src="./fonts/anytime.png" >`;
+            }
+
+
+
+
         $(".main").append(`<div class="${current.name} player">
-            <h1>${current.name}: <span class="${current.name}Gold ${color}-text">${current.gold}g</span> </h1>
+            <h1><span class="playerName">${current.name}:</span><br> ${actionsString} <br>
+            
+            ${anytimesString}<br>
+            
+            <span class="${current.name}Gold ${color}-text">${current.gold}g</span><br>
+             
+             <span class="${current.name}Stamina ${stamColor}-text">${current.stamina} Stamina</span><br>
+             </h1>
             </div>`);
 
-        $(`.${current.name}`).mousedown(function (e) {
+        $(`.${uid}`).mousedown(function (e) {
+            // alert("Clicked?");
+            if(e.which === 1){
+                current.actionsLeft.splice(current.actionsLeft.indexOf(this.id), 1);
+            }else if(e.which === 2){
+
+            }else{
+                current.actionsLeft.push(this.id);
+            }
+            updateChars();
+        });
+        $(`.anytime`).mousedown(function (e) {
+            if(e.which === 1){
+                current.anytimesLeft--;
+            }
+            updateChars();
+        });
+
+        $(".gem").css("height", $(".playerName").height()*1.5);
+        $(".gem").css("width", $(".playerName").height()*1.5);
+        $(".anytime").css("height", $(".playerName").height()*1.5);
+        $(".anytime").css("width", $(".playerName").height()*1.5);
+        // $(".gem").css("margin-top", $(".playerName").height() + "px");
+
+        $(`.${current.name}Gold`).mousedown(function (e) {
             if(e.which === 2){
-                this.remove();
-                chars.splice(chars.indexOf(this), 1);
+                // this.remove();
+                // chars.splice(chars.indexOf(this), 1);
             } else if(e.which === 1){
 
 
@@ -55,6 +116,37 @@ let updateChars = () => {
             updateChars();
         });
 
+        $(`.${current.name}Stamina`).mousedown(function (e) {
+            if(e.which === 2){
+                current.stamina = current.maxstamina;
+            } else if(e.which === 1){
+
+
+                if(holding.indexOf(16) !== -1){
+                    current.stamina -= 10;
+                }else if(holding.indexOf(17) !== -1) {
+                    console.log("ctrlclik");
+                    current.stamina -= 5;
+                }else{
+                    current.stamina -= 1;
+                }
+            }else{
+
+
+                if(holding.indexOf(16) !== -1){
+                    current.stamina += 10;
+                }else if(holding.indexOf(17) !== -1) {
+                    console.log("ctrlclik");
+                    current.stamina += 5;
+                }else{
+                    current.stamina += 1;
+                }
+            }
+
+            updateChars();
+        });
+
+
         if(current.gold > 0){
             console.log(`${current.name}Gold`);
             $(`.${current.name}Gold`).addClass("text-green");
@@ -67,9 +159,13 @@ let updateChars = () => {
 
 $(document).ready(function () {
     $(".addCharBtn").click(function () {
-        chars.push(new player(prompt("Enter a name for new player")));
+        let name = prompt("Enter a name for new player");
+        if(name !== undefined && name !== "" && name !== null){
+            chars.push(new player(name));
 
-        updateChars();
+            updateChars();
+        }
+
     });
 
 
@@ -88,7 +184,44 @@ $(document).ready(function () {
         }
 
         if(e.keyCode === 67){
-            alert("Press g to set gold values. Left click to add gold, right click to subtract. Shift modifies balance by 50, a normal click by 10, and a control click by 5");
+            alert("GOLD CONTROLS: Press g to set gold values. Left click gold count to add gold, right click to subtract. Shift modifies balance by 50, a normal click by 10, and a control click by 5");
+            alert("STAMINA CONTROLS: Press s to set max stamina values, middle click stamina to full heal, click to remove 1 stamina, shift click to remove 10 stamina, control click to remove 5 stamina, right click to add 1 stamina, shift+right click to add 10 stamina, ctrl+right click to add 5 stamina");
+            alert("GEM CONTROLS: Left click to remove gem, right click to temporarily add. Press R to reset gems for the round, press L or reset gems for the combat encounter")
+        }
+
+
+        if(e.keyCode === 83){
+            for(let i = 0; i < chars.length; i++) {
+                let current = chars[i];
+
+                current.maxstamina = parseInt(prompt(`Set MAXIMUM stamina for ${current.name}, currently ${current.maxstamina}`));
+            }
+            updateChars();
+        }
+        if(e.keyCode === 82){
+            for(let i = 0; i < chars.length; i++) {
+                let current = chars[i];
+                // alert(current.defaults);
+                let someArray = current.baseActions;
+                console.log(someArray);
+                if(current.actionsLeft.indexOf("once") !== -1){
+                    current.actionsLeft = ["move","normal","once"];
+                }else{
+                    current.actionsLeft = ["move","normal"];
+                }
+
+            }
+            updateChars();
+        }
+        if(e.keyCode === 76){
+            for(let i = 0; i < chars.length; i++) {
+                let current = chars[i];
+                if(current.actionsLeft.indexOf("once") === -1){
+                    current.actionsLeft.push("once");
+                }
+                current.anytimesLeft = 3;
+            }
+            updateChars();
         }
     })
 });
