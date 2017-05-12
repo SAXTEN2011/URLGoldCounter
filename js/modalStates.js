bc16088ee77632af59364365bf79c1f7765e7931
Aaron <amaxpierce@yahoo.com>
2017-05-11 21:44:46 -0500

    added player context menu

---
+++ js/modalStates.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Aaron on 5/11/2017.
+ */
+let defaultstate = `
+    <div class="modal-content defaultState">
+        <h4 class="modalPlayerName">null</h4>
+        <a class="waves-effect waves-light btn purple darken" onclick="">Set Max Stamina</a>
+    </div>
+`;
+
+let setMaxStaminaState = `
+
+`;
